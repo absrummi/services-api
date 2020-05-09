724375fb6c12d1acdf2e56c5fe5fbc2522a74b22
Saqib sattar <saqibkhanz1234@gmail.com>
2020-05-09 20:39:03 +0500

    Adding role field in User.js model

--- apis/models/user.js
+++ apis/models/user.js
@@ -8,7 +8,11 @@ const userSchema = mongoose.Schema({
         unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {
+       type: String,
+       default: 'user'
+   }
 });
 
 module.exports = mongoose.model('Users', userSchema);
